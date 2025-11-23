98b140b876a58687b264863cbc9292435958b7fd
larsssmoatsss <moats.lars@gmail.com>
2025-11-22 23:18:26 -0500

    fix database connection deployment issue

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:6cznSN%269Cxn9%23RG@db.apbnnemyipgbihqemxoh.supabase.co:5432/postgres',
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:6cznSN%269Cxn9%23RG@db.apbnnemyipgbihqemxoh.supabase.co:5432/postgres',
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  // Force IPv4
+  options: '-c search_path=public'
 });
 
 module.exports = pool;
